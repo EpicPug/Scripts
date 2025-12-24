686eadad12290135b9867c9c729d92c8058e1670
EpicPug <112693990+EpicPug@users.noreply.github.com>
2025-12-24 17:45:39 -0600

    Update AutoFarm.lua

--- AutoFarm.lua
+++ AutoFarm.lua
@@ -1,15 +1,5 @@
+--!strict
+
 if not game:IsLoaded() then
-	repeat
-		task.wait(0.5)
-	until game:IsLoaded()
-end
-
-local CoreGui = game:GetService("CoreGui")
-if CoreGui:FindFirstChild("Pug's Auto Farm") then
-	return
-end
-
-local DA_HOOD_ID = 2788229376
-if game.PlaceId ~= DA_HOOD_ID then
-	return
+	game.Loaded:Wait()
 end
